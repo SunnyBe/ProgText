83580255684f534ada224c500006a3aa3f2a7808
Zistus Ndu <sunday@ajocard.com>
2020-04-21 01:31:43 +0100

    Initial setup

---
+++ app/src/main/java/com/zistus/progtext/view/ProgText.kt
@@ -0,0 +1,13 @@
+package com.zistus.progtext.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class ProgText @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
