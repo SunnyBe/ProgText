eb3fce02a082070869a4556c142e99abe69172a5
Zistus Ndu <sunday@ajocard.com>
2020-04-22 11:46:52 +0100

    Set up the ProgText view class

--- app/src/main/java/com/zistus/progtext/view/ProgText.kt
+++ app/src/main/java/com/zistus/progtext/view/ProgText.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
+/**
+ * Build by ndusunday@gmail.com
+ * To show progress with texts and a ball dropping on each text.
+ */
+
 class ProgText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
